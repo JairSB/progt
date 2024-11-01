9292a6b70b62c436a5110338d46195dec9a3499b
JairSB <benarrozjair@gmail.com>
2024-11-01 17:37:10 -0300

    so falta um detalhe

--- progto/jsss/user.js
+++ progto/jsss/user.js
@@ -1,11 +1,16 @@
 const url_cad = "https://bora-14220-default-rtdb.firebaseio.com/";
 const id_usuario = localStorage.getItem("chave");
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+
+let crotole = "";
 
 fetch(url_cad+'user/'+id_usuario+'.json')
 .then(resposta => resposta.json())
 .then(dados =>{
-    usuarionome.innerHTML += dados.nome;
+    usuarionome.innerHTML = dados.nome;
 })
 
 try{
@@ -14,17 +19,40 @@ try{
  console.log('0mapas')
 }
 
-adiciona_predio.addEventListener('click', e=>{
-  predio ={
-    'nome':nome.value,
-    'tipo':tipo.value,
-    'descricao':descricao.value,
-    'cep':cep.value
-  }
-  fetch(url_cad+'user/'+id_usuario+'/predio.json',{method:'POST',body:JSON.stringify(predio)})
-  mapear_predios()
+offpg.addEventListener('click',e=>{
+  localStorage.clear();
+  window.location.href="http://127.0.0.1:5500/ht/Cocecta.html";
 })
 
+adiciona_predio.addEventListener('click', e=>{
+  fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+  .then(resposta => resposta.json())
+  .then(dados =>{
+
+    predio ={
+      'nome':nome.value,
+      'tipo':tipo.value,
+      'descricao':descricao.value,
+      'casa_nu':casa_nu.value,
+      'cep':cep.value,
+      'rua':dados.logradouro,
+      'bairro':dados.bairro,
+      'clicks':0
+    }
+    if(crotole ==""){
+    fetch(url_cad+'user/'+id_usuario+'/predio.json',{method:'POST',body:JSON.stringify(predio)})
+    mapear_predios()
+  }else{
+    fetch(url_cad+'user/'+id_usuario+'/predio/'+crotole+'.json',{
+      method: 'PUT', body: JSON.stringify(predio)})
+      mapear_predios()
+      crotole="";
+  }
+
+})
+
+})
+  
 function mapear_predios(){
   mapas.innerHTML ="";
   fetch(url_cad+'user/'+id_usuario+'/predio.json')
@@ -53,7 +81,6 @@ function mapear_predios(){
 
 }
 
-
 function deleta(n) {
     fetch(url_cad+'user/'+id_usuario+'/predio/' + n + '.json', { method: "DELETE" })
     .then(resposta=>resposta.json())    
@@ -62,4 +89,51 @@ function deleta(n) {
         )
         )
 
+}
+
+function muda(n){
+  fetch(url_cad+'user/'+id_usuario+'/predio/' + n + '.json')
+    .then(resposta=>resposta.json())    
+    .then(dados=>{
+      nome.value=dados.nome
+      tipo.value=dados.tipo
+      descricao.value=dados.descricao
+      casa_nu.value=dados.casa_nu
+      cep.value=dados.cep
+      crotole = n
+      document.getElementById('formulario').click();
+    }
+      )
+}
+
+
+drawChart()
+
+function drawChart() {
+  var grafi_predios =[]
+  fetch(url_cad+'user/'+id_usuario+'/predio.json')
+  .then(resposta => resposta.json())
+  .then(dados =>{
+    for (const key in dados) {
+      grafi_predios.push([dados[key].nome ,dados[key].clicks ])
+    }
+    
+  })
+
+  var data = google.visualization.arrayToDataTable([
+    grafi_predios
+  ]);
+  
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+
+
+
+  var options = {
+    title: 'visitas pelo app'
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  chart.draw(data, options);
 }
